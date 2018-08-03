1360958a5921eb9c513f4a12de073989a499589c
thextool <matthew.riach@gmail.com>
2018-08-03 12:10:40 -0600

    :sparkles: added error handling

--- server/app.js
+++ server/app.js
@@ -12,5 +12,20 @@ app.get('/', (req, res) => {
     res.send('Hello Earth!');
 })
 
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    res.status(404);
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: req.app.get("env") === "development" ? err.stack : {}
+    });
+});
+
 module.exports = app
 
